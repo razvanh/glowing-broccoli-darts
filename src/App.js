c23dd7fcea2d6adde46c298f633cc4ce3b7bd5d7
Razvan Horeanga <razvan.horeanga@gmail.com>
2018-03-15 22:53:01 -0500

    Added state for the input box text

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state ={
+            scores:[],
+            text:''
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({text: e.target.value});
+    }
+
   render() {
     return (
         <div>
@@ -15,6 +29,7 @@ class App extends Component {
                 </label>
                 <input
                     id="new-score"
+                    onChange={this.handleChange}
                 />
                 <button>
                     Add to rankings
