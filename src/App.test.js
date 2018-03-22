fe1d6903fbbbad2a12e56a2afb55cfe474d7c8ea
Razvan Horeanga <razvan.horeanga@gmail.com>
2018-03-22 15:06:32 -0500

    Added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {shallow} from 'enzyme';
+import Enzyme,{shallow,mount} from 'enzyme';
+import Adapter from "enzyme-adapter-react-16";
 import App from './App';
+import {handleSubmit} from './App';
+import ScoreList from './App';
 require('jest-localstorage-mock');
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+Enzyme.configure({ adapter: new Adapter() });
 
+const scores = [
+    {
+        id:1,
+        name:'Razvan',
+        score:100
+    }
+];
+
+const sample_scores = [
+
+];
+
+describe('<App />', () => {
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('can set scores state in App component and count state number', () => {
+        const wrapper = shallow(<App />);
+        wrapper.setState({ scores: scores });
+        expect(wrapper.state('scores').length).toBe(1);
+    });
+
+    it('can render ScoreList in App component', () => {
+        const wrapper = mount(<App />);
+        expect(wrapper.find(ScoreList).type()).toBe(ScoreList);
+    });
+
+});
 
