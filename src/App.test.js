98be2db2877cbd8455ab9a25114fe6362369faca
Razvan Horeanga <razvan.horeanga@gmail.com>
2018-03-22 16:12:04 -0500

    Added test to check correct display of the leaderboard

--- src/App.test.js
+++ src/App.test.js
@@ -40,5 +40,42 @@ describe('<App />', () => {
         expect(wrapper.find(ScoreList).type()).toBe(ScoreList);
     });
 
+
+    it('should display leaderboard correctly', () => {
+        const fakeEvent = { preventDefault: () => {}
+        };
+        const wrapper = mount(<App />);
+        expect(wrapper.find('#new-score-form').length).toBe(1);
+
+        wrapper.setState({ text: 'Janet Pluchinsky, 10' });
+        wrapper.find('#new-score-form button').simulate('submit', fakeEvent);
+
+        wrapper.setState({ text: 'Eliot Waugh, 1' });
+        wrapper.find('#new-score-form button').simulate('submit', fakeEvent);
+
+        wrapper.setState({ text: 'Alice Quinn, 27' });
+        wrapper.find('#new-score-form button').simulate('submit', fakeEvent);
+
+        wrapper.setState({ text: 'Julia Wicker, 7' });
+        wrapper.find('#new-score-form button').simulate('submit', fakeEvent);
+
+        wrapper.setState({ text: 'Quentin Coldwater, 0' });
+        wrapper.find('#new-score-form button').simulate('submit', fakeEvent);
+
+        wrapper.setState({ text: 'Janet Pluchinsky, 20' });
+        wrapper.find('#new-score-form button').simulate('submit', fakeEvent);
+
+        wrapper.setState({ text: 'Julia Wicker, 20' });
+        wrapper.find('#new-score-form button').simulate('submit', fakeEvent);
+
+        wrapper.setState({ text: 'Jollyby, 42' });
+        wrapper.find('#new-score-form button').simulate('submit', fakeEvent);
+
+
+        expect(wrapper.state('scores').length).toBe(6);
+
+        expect(wrapper.find('ul').html()).toBe('<ul><li class="leaderboard"><span class="leaderboard__rank">1</span><span class="leaderboard__name">Jollyby</span><span class="leaderboard__score">42 pts</span></li><li class="leaderboard"><span class="leaderboard__rank">2</span><span class="leaderboard__name">Janet Pluchinsky</span><span class="leaderboard__score">30 pts</span></li><li class="leaderboard"><span class="leaderboard__rank">3</span><span class="leaderboard__name">Alice Quinn</span><span class="leaderboard__score">27 pts</span></li><li class="leaderboard"><span class="leaderboard__rank">3</span><span class="leaderboard__name">Julia Wicker</span><span class="leaderboard__score">27 pts</span></li><li class="leaderboard"><span class="leaderboard__rank">5</span><span class="leaderboard__name">Eliot Waugh</span><span class="leaderboard__score">1 pts</span></li><li class="leaderboard"><span class="leaderboard__rank">6</span><span class="leaderboard__name">Quentin Coldwater</span><span class="leaderboard__score">0 pts</span></li></ul>');
+        });
+
 });
 
